d932ddf88286b292b050d0f51f5f33255445697a
Boris Castellanos Matamoros <b.castellanos@uniandes.edu.co>
2020-10-21 15:37:32 -0500

    taller typescript

---
+++ dataCourses.ts
@@ -0,0 +1,14 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de Sw", "Cesar Augusto Forero", 3),
+  new Course("Algebra Lineal", "Carolina Benedetti", 3),
+  new Course("Frances 1", "Jenny Muñoz", 3),
+  new Course("Sistemas Transaccionales", "German Bravo", 3),
+  new Course("Arquitectura Empresarial", "Oscar Gonzales", 3),
+  new Course("Fisica 2", "Mayerlin Nuñez", 3),
+  new Course("Fisica Experimental 2","Sandra Gonzalez",1),
+  new Course("Drift or think", "Gustavo Gonzalez", 2),
+  new Course("Understanding Society", "Gregory Lobo", 2)
+]
+
